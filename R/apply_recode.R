dd54f29d994088f6a01ff8e79530486997c27529
Daniel Casey <dcasey@kingcounty.gov>
2019-07-18 11:03:30 -0700

    move back to labelled from lfactors

--- R/apply_recode.R
+++ R/apply_recode.R
@@ -18,7 +18,7 @@
 #' @export
 #'
 #' @return a new column with the recoded values whose class is dependent on the recode instructions.
-#' If a recode instruction is passed that is not relevant to the dataset (e.g. year), a column of NAs will be returned.
+#'
 #'
 #'
 apply_recode = function(data, year, recode, jump_scope = F, return_vector = F){
@@ -69,124 +69,141 @@ apply_recode = function(data, year, recode, jump_scope = F, return_vector = F){
       }
     }
 
-  }
 
-  #construct recoding
-  old = data[, get(recode$old_var)]
 
-  #coerce old to be the class of the specified old values
-  #assuming no new NAs
-  if(!inherits(old, class(recode$old_value))){
-    old_class = class(old)
+    #construct recoding
+    old = data[, get(recode$old_var)]
 
-    start_na = sum(is.na(old))
-    old = as(old, class(recode$old_value))
-    end_na = sum(is.na(old))
+    #coerce old to be the class of the specified old values
+    #assuming no new NAs
+    if(!inherits(old[[1]], class(recode$old_value))){
+      old_class = class(old)
 
-    if(end_na > start_na){
-      stop(paste('Could not cleanly coerce column --', recode$old_var, '-- to', class(recode$old_value), 'for recoding. Check mismatch between old_var and class(old_value) in recode'))
+      start_na = sum(is.na(old))
+      old = as(old, class(recode$old_value))
+      end_na = sum(is.na(old))
+
+      if(end_na > start_na){
+        stop(paste('Could not cleanly coerce column --', recode$old_var, '-- to', class(recode$old_value), 'for recoding. Check mismatch between old_var and class(old_value) in recode'))
+      }
+
+      #coerce ret as well
+      ret = as(ret, class(recode$old_value))
+
+      #strip lingering labels
+      if(any(old_class %in% 'factor')){
+        attr(ret, 'levels') <- NULL
+      }
+
     }
 
-    #coerce ret as well
-    ret = as(ret, class(recode$old_value))
+    for(i in seq_along(recode$old_value)){
 
-  }
+      #binning instructions
+      if(bin_me & check_bin(recode$old_value[i])){
 
-  for(i in seq_along(recode$old_value)){
+        #parse the instructions for recoding
+        left = substr(recode$old_value[i],1,1)
+        right = substr(recode$old_value[i],nchar(recode$old_value[i]),nchar(recode$old_value[i]))
+        inner = substr(recode$old_value[i], 2, nchar(recode$old_value[i]) - 1)
 
-    #binning instructions
-    if(bin_me & check_bin(recode$old_value[i])){
+        minmax = strsplit(inner, ',', fixed = T)
 
-      #parse the instructions for recoding
-      left = substr(recode$old_value[i],1,1)
-      right = substr(recode$old_value[i],nchar(recode$old_value[i]),nchar(recode$old_value[i]))
-      inner = substr(recode$old_value[i], 2, nchar(recode$old_value[i]) - 1)
+        if(length(minmax) != 1){
+          stop(paste0('Expecting one comma `,` in the old_value recode instructions. Found:' ,length(minmax)))
+        }
 
-      minmax = strsplit(inner, ',', fixed = T)
+        minmax = as.numeric(trimws(minmax[[1]]))
 
-      if(length(minmax) != 1){
-        stop(paste0('Expecting one comma `,` in the old_value recode instructions. Found:' ,length(minmax)))
+        #check if the coercian created any errors. if so, stop
+        if(any(is.na(minmax))){
+          stop('Binning operation coerced to NA bounds')
+        }
+
+        if(left == '(' & right == ')'){
+          ret[which(old > minmax[1] & old< minmax[2])] = recode$new_value[i]
+        }else if(left == '[' & right == ')'){
+          ret[which(old >= minmax[1] & old< minmax[2])] = recode$new_value[i]
+        }else if(left == '(' & right == ']'){
+          ret[which(old > minmax[1] & old <= minmax[2])] = recode$new_value[i]
+        }else{
+          ret[which(old >= minmax[1] & old<= minmax[2])] = recode$new_value[i]
+        }
+
+      }else{ #normal recoding
+        ret[which(old %in% recode$old_value[i])] = recode$new_value[i]
       }
+    }
 
-      minmax = as.numeric(trimws(minmax[[1]]))
-
-      #check if the coercian created any errors. if so, stop
-      if(any(is.na(minmax))){
-        stop('Binning operation coerced to NA bounds')
-      }
-
-      if(left == '(' & right == ')'){
-        ret[which(old > minmax[1] & old< minmax[2])] = recode$new_value[i]
-      }else if(left == '[' & right == ')'){
-        ret[which(old >= minmax[1] & old< minmax[2])] = recode$new_value[i]
-      }else if(left == '(' & right == ']'){
-        ret[which(old > minmax[1] & old <= minmax[2])] = recode$new_value[i]
+    #apply labels if needed
+    if(!is.null(recode$new_label)){
+      new_labs = unique(data.table(value = recode$new_value, label = as.character(recode$new_label), type = 'new'))
+      valclass = class(recode$new_value)
+    }else{
+      new_labs = data.table()
+      valclass = 'numeric'
+    }
+    if(is.labelled(data[1, get(recode$old_var)]) | is.factor(data[1, get(recode$old_var)])){
+      ov = recode$old_var
+      ifact = is.factor(data[1, get(recode$old_var)])
+      #Extract values and labels pair
+      if(ifact){
+        old_labs = unique(data[, .(value = as(get(ov), valclass),
+                                 label = as.character(get(ov)),
+                                                type = 'old')])
       }else{
-        ret[which(old >= minmax[1] & old<= minmax[2])] = recode$new_value[i]
+        old_labs = data.table(value = val_labels(data[1, get(recode$old_var)]),
+                              label = names(val_labels(data[1, get(recode$old_var)])),
+                              type = 'old')
+      }
+    }else{
+      old_labs = data.table()
+    }
+    #identify duplicates in labeling. Use new unless its NA and old is not
+    labs = unique(rbind(old_labs, new_labs))
+
+    if(nrow(labs)>0){
+
+      badlabs = labs[type == 'new', .N, by = 'value'][N>1, value]
+      if(length(badlabs) > 0 ){
+        stop(paste('The following values are mapped to more than new label:', paste0(badlabs, collapse = ' | ')))
       }
 
-    }else{ #normal recoding
-      ret[which(old %in% recode$old_value[i])] = recode$new_value[i]
+      labs = dcast(labs, value ~ type, value.var = 'label')
+      labs[, label := new]
+
+      #only attempt to carry over old labels if replacing an existing variable
+      if(nrow(old_labs)>0 & (recode$new_var == recode$old_var)) labs[is.na(label), label:= old]
+
+      #if the labels are all NA, replace with value
+      if(nrow(labs) == nrow(labs[is.na(label)])){
+        labs[,label:= value]
+      }
+
+      #keep only labels that are represented in the data
+      labs = labs[value %in% unique(ret), ]
+      setorder(labs, value)
+      #throw an error if the labels are mapping two numbers to the same value
+      #if(any())
+
+      #drop value:label pair of NA:NA
+      labs = labs[!(is.na(value) & is.na(label)),]
+
+      doublabs = labs[, .N, by = 'label'][N>1,label]
+      if(length(doublabs) > 0){
+        stop(paste('The following pairs of values:labels indicate duplicate mappings.',
+                   'This is likely caused by a partial overwrite of a variable (e.g. old_var == new_var).',
+                   paste(labs[label %in% doublabs, paste0(value, ':', label)], collapse = ", ")))
+      }
+
+      #if a numeric with labels, transfer to numeric. Otherwise leave it as numeric, character or whatever.
+      if(is.numeric(ret)){
+        new_labs = labs[,value]
+        names(new_labs) = labs[, label]
+        ret = labelled::labelled(ret, new_labs)
+      }
     }
   }
-
-  #apply labels if needed
-  if(!is.null(recode$new_label)){
-    new_labs = unique(data.table(value = recode$new_value, label = as.character(recode$new_label), type = 'new'))
-    valclass = class(recode$new_value)
-  }else{
-    new_labs = data.table()
-    valclass = 'numeric'
-  }
-  if(inherits(data[, get(recode$old_var)], 'factor')){
-    ov = recode$old_var
-    old_labs = unique(data[, .(value = as(get(ov), valclass), label = as.character(get(ov)), type = 'old')])
-  }else{
-    old_labs = data.table()
-  }
-  #identify duplicates in labeling. Use new unless its NA and old is not
-  labs = unique(rbind(old_labs, new_labs))
-
-  if(nrow(labs)>0 & !simple_rename){
-
-    badlabs = labs[type == 'new', .N, by = 'value'][N>1, value]
-    if(length(badlabs) > 0 ){
-      stop(paste('The following values are mapped to more than new label:', paste0(badlabs, collapse = ' | ')))
-    }
-
-    labs = dcast(labs, value ~ type, value.var = 'label')
-    labs[, label := new]
-
-    #only attempt to carry over old labels if replacing an existing variable
-    if(nrow(old_labs)>0 & (recode$new_var == recode$old_var)) labs[is.na(label), label:= old]
-
-    #if the labels are all NA, replace with value
-    if(nrow(labs) == nrow(labs[is.na(label)])){
-      labs[,label:= value]
-    }
-
-    #keep only labels that are represented in the data
-    labs = labs[value %in% unique(ret), ]
-    setorder(labs, value)
-    #throw an error if the labels are mapping two numbers to the same value
-    #if(any())
-
-    #drop value:label pair of NA:NA
-    labs = labs[!(is.na(value) & is.na(label)),]
-
-    doublabs = labs[, .N, by = 'label'][N>1,label]
-    if(length(doublabs) > 0){
-      stop(paste('The following pairs of values:labels indicate duplicate mappings.',
-                 'This is likely caused by a partial overwrite of a variable (e.g. old_var == new_var).',
-                 paste(labs[label %in% doublabs, paste0(value, ':', label)], collapse = ", ")))
-    }
-
-    #if a numeric with labels, transfer to numeric. Otherwise leave it as numeric, character or whatever.
-    if(is.numeric(ret)){
-      ret = lfactors::lfactor(ret, labs[,value], labs[, label])
-    }
-  }
-
   #prepare output
   if(jump_scope){
     data[, (recode$new_var) := ret]
--- tests/testthat/test_apply_recode.R
+++ tests/testthat/test_apply_recode.R
@@ -70,7 +70,7 @@ test_that('Recoding numeric to have labels',{
   a = create_recode('a','b', old_value = 1:5, new_value = 1:5, new_label = letters[1:5])
   d = data.table(a = 1:5)
   r = apply_recode(data = d, year = 2018, recode = a, jump_scope = F, return_vector = T)
-  expect_equal(factor(1:5, 1:5, letters[1:5]), r)
+  expect_equal(labelled::labelled(1:5, c(a = 1, b = 2, c = 3, d = 4, e = 5)), r)
 })
 
 #recoding over an existing factor, complete replacement
@@ -78,14 +78,14 @@ test_that('Recoding over factor variable-- specifing numeric representation',{
   a = create_recode('a','a', old_value = 1:3, new_value = 1:3, new_label = c('d','e','f'))
   d = data.table(a = factor(1:3, 1:3, c('a','b','c')))
   r = apply_recode(data = d, year = 2018, recode = a, jump_scope = F, return_vector = T)
-  expect_equal(lfactors::lfactor(1:3, 1:3, letters[4:6]), r)
+  expect_equal(labelled::labelled(as.numeric(1:3), c(d = 1, e = 2, f = 3)), r)
 })
 
 test_that('Recoding over factor variable-- specifing numeric representation and changing the underlying numeric',{
   a = create_recode('a','a', old_value = 1:3, new_value = 4:6, new_label = c('d','e','f'))
   d = data.table(a = factor(1:3, 1:3, c('a','b','c')))
   r = apply_recode(data = d, year = 2018, recode = a, jump_scope = F, return_vector = T)
-  expect_equal(lfactors::lfactor(4:6, 4:6, letters[4:6]), r)
+  expect_equal(labelled(4:6, c(d = 4, e = 5, f = 6)), r)
   expect_equal(as.numeric(r), 4:6)
 })
 
@@ -102,99 +102,15 @@ test_that('Recoding over factor variable-- partial update',{
   a = create_recode('a','a', old_value = 1:2, new_value = c(9,10), new_label = c('a','b'))
   d = data.table(a = factor(1:3, 1:3, c('a','b','c')))
   r = apply_recode(data = d, year = 2018, recode = a, jump_scope = F, return_vector = T)
-  expect_equal(lfactors::lfactor(c(9, 10, 3), c(3,9,10), c('c', 'a','b')), r)
+  expect_equal(labelled(c(9, 10, 3), c(c = 3, a = 9,b = 10)), r)
 })
 
-#do some tests with lfactors
-test_that('Recoding over factor variable-- specifing numeric representation',{
-  a = create_recode('a','a', old_value = 1:3, new_value = 1:3, new_label = c('d','e','f'))
-  d = data.table(a = lfactors::lfactor(1:3, 1:3, c('a','b','c')))
+
+test_that('Labelled in Labelled out with shifting values',{
+  a = create_recode('a', 'a', old_value = 1:3, new_value = 4:6, new_label = c('d','e','f'))
+  d = data.table(a = labelled(1:3, c(a = 1, b = 2, c = 3)))
   r = apply_recode(data = d, year = 2018, recode = a, jump_scope = F, return_vector = T)
-  expect_equal(lfactors::lfactor(1:3, 1:3, letters[4:6]), r)
-})
-
-test_that('Recoding over factor variable-- specifing numeric representation and changing the underlying numeric',{
-  a = create_recode('a','a', old_value = 1:3, new_value = 4:6, new_label = c('d','e','f'))
-  d = data.table(a = lfactors::lfactor(1:3, 1:3, c('a','b','c')))
-  r = apply_recode(data = d, year = 2018, recode = a, jump_scope = F, return_vector = T)
-  expect_equal(lfactors::lfactor(4:6, 4:6, letters[4:6]), r)
-  expect_equal(as.numeric(r), 4:6)
-})
-
-test_that('Recoding over factor variable-- reassign a label but lose 1:1 value:label mapping',{
-  a = create_recode('a','a', old_value = 1:2, new_value = c(9,10), new_label = c('a','c'))
-  d = data.table(a = lfactors::lfactor(1:3, 1:3, c('a','b','c')))
-  expect_error(apply_recode(data = d, year = 2018, recode = a, jump_scope = F, return_vector = T),
-               "The following pairs of values:labels indicate duplicate mappings. This is likely caused by a partial overwrite of a variable (e.g. old_var == new_var). 3:c, 10:c",
-               fixed = T)
-
-})
-
-test_that('Recoding over factor variable-- partial update',{
-  a = create_recode('a','a', old_value = 1:2, new_value = c(9,10), new_label = c('a','b'))
-  d = data.table(a = lfactors::lfactor(1:3, 1:3, c('a','b','c')))
-  r = apply_recode(data = d, year = 2018, recode = a, jump_scope = F, return_vector = T)
-  expect_equal(lfactors::lfactor(c(9, 10, 3), c(3,9,10), c('c', 'a','b')), r)
+  expect_equal(labelled(4:6, c(d = 4, e = 5, f = 6)), r)
 })
 
 
-
-
-# test_that('Recoding multiple values at once, all non-NA, adding labels with padding',{
-#   a = create_recode('a','b', old_value = 1:3, new_value = c('3','4','5'), new_label = c('a','b','c'))
-#   d = data.table::data.table(a = 1:3)
-#   r = apply_recode(data = d, year = 2018, recode = a, jump_scope = F)
-#   expect_equal(as.numeric(r[, b]), 4:6)
-#   expect_equal(as.character(r[,b]), c('a','b','c'))
-# })
-
-#Multiple 1 : 1 numeric recodes at once
-
-#Numeric recoding using binning
-
-#Test for side effect of adding a column
-
-#Simple recode with an overwrite
-
-#Simple recode with an overwrite that jumps from numeric -> character
-
-#Simple recode overwritting factor labels
-
-#Simple recode adding new values and labels
-
-#Binned recoding of factors
-
-#Expected error of passing instructions
-
-#figure out what to do with factors and how we can preserve the underlying numeric representation of the variable
-#e.g. is a factor of 2, 3, 4 represented as 1:3?
-
-#simple recode tests
-a = create_recode('V1', 'V2', 'a', 'b', 'b', '', '')
-b = data.table('a')
-t1 = apply_recode(b, 2016, a, F)
-t2 = apply_recode(b, 2016, a, T)
-
-#more complex
-data_vector = factor(rep(letters[1:3],3),letters[1:3])
-old_vals = letters[1:2]
-new_vals = LETTERS[1:2]
-d = data.table(V1 = data_vector)
-
-#creating a new column
-rec = create_recode('V1', 'V2', old_vals, new_vals, new_label = new_vals)
-e = apply_recode(d, 2016, rec)
-
-#overwriting and existing one
-d1 = copy(d)
-d1[, V2:=V1]
-rec = create_recode('V1', 'V2', old_vals, new_vals, new_label = new_vals)
-e1 = apply_recode(d1, 2016, rec)
-
-#recoding with binning
-d2 = data.table(v1 = c(NA, 1:10))
-rec2 = create_recode('v1', 'v2', old_value = c('[1,2]','(3,4]', '(4,6)', '[6,7)', 8, 9, NA),
-                                 new_value = c(-1,-2,-3,-4,-5,-6,-7))
-e2 = apply_recode(d2, 2016, rec2)
-
-#what happens when try to recode a factor(e.g. the label) rather than the value?
