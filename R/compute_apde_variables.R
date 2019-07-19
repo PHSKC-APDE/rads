d8dbd72a4abbb6b7dd1059ed860637184e3b4820
Daniel Casey <dcasey@kingcounty.gov>
2019-07-19 14:05:12 -0700

    tests have been moved to svy_new

--- R/compute_apde_variables.R
+++
@@ -1,9 +0,0 @@
-#' compute_apde_vars
-#'
-#' Takes the raw HYS data and year specific HYS crosswalk sheet. Applies value labels to the variables and adds the variable labels to the attributes
-#'
-#' @param input data.table. HYS data after formatting (see \code{format_single_year})
-#'
-#' @import data.table
-#' @export
-#'
