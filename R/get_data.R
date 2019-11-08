2bb2c24c788a88225579c2ed608d9593dbdf78f2
Danny Colombara <dcolombara@kingcounty.gov>
2019-11-08 10:52:15 -0800

    Added get_data_birth() to pull SQL birth data

--- R/get_data.R
+++ R/get_data.R
@@ -99,3 +99,48 @@ get_data_hys <- function(cols = NA, year = c(2016, 2018), weight_variable = 'kcf
   return(svy)
 }
 
+
+
+#' Get Birth microdata from storage.
+#'
+#' @param cols Character vector of length >=1. Identifies which columns should be returned. NA returns all columns in the analytic dataset.
+#'     See \code{\link{list_dataset_columns}} for more information on which columns are considered default by dataset.
+#' @param year Numeric vector. Identifies which years of data should be pulled
+#' @param kingco logical. Return dataset for analyses where mother's residence is in King County only.
+#'
+#' @return dataset either data.table (adminstrative data) for further analysis/tabulation
+#'
+#' @import data.table
+#' @importFrom labelled to_factor
+#' @export
+#'
+#' @examples
+#'
+#' \dontrun{
+#'  get_data_birth(cols = NA, year = c(2015, 2016, 2017), kingco = F)
+#' }
+get_data_birth <- function(cols = NA, year = c(2017),  kingco = T){
+  # pull columns and years from sQL
+  ifelse(is.na(cols), cols <- "*", cols <- paste(cols, collapse=", "))
+  
+  query.string <- glue:: glue_sql ("SELECT ",  cols, " FROM [PH_APDEStore].[final].[bir_wa] 
+                                   WHERE chi_year IN (",  paste(year, collapse=", "), ")")  
+  
+  if(kingco == T){query.string <- glue:: glue_sql (query.string, " AND chi_geo_kc = 1")}
+  
+  db.apde50 <- odbc::dbConnect(odbc::odbc(), "PH_APDEStore50")
+  dat <- data.table::setDT(DBI::dbGetQuery(db.apde50, query.string))
+  odbc::dbDisconnect(db.apde50)
+  
+  # Format string variables due to SQL import quirks
+  original.order <- names(dat)
+  string.columns <- sapply(dat,is.character) # identify string columns as a logical vector
+  string.columns <- names(dat[, ..string.columns]) # identify string columns as a character vector
+  dat <- dat[, (string.columns) := lapply(.SD, trimws,which="r"), .SDcols = string.columns] # trim white space to right
+  dat <- dat[, (string.columns) := lapply(.SD, factor), .SDcols = string.columns] # convert strings to factors
+  
+  # reorder table
+  setcolorder(dat, original.order)
+  
+  return(dat)
+}
