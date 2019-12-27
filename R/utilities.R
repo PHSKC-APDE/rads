419d4b55cfcfd3aa12f117bc6a3bf67c32ea8bff
Danny Colombara <dcolombara@kingcounty.gov>
2019-12-27 13:46:40 -0800

    updated utilities

--- R/utilities.R
+++ R/utilities.R
@@ -1,4 +1,4 @@
-#' Returns the list of datasets currently available for analysis in RADS
+#' Returns the list of datasets currently available for RADS
 #'
 #' @return Character vector of available datasets.
 #' @export
@@ -8,20 +8,18 @@
 #'  list_apde_data()
 #' }
 list_apde_data <- function(){
-
+  
   ret <- c('hys', 'birth', 'bsk')
-
+  
   return(ret)
-
-
+  
+  
 }
 
-#' List columns available for analysis for a particular dataset in RADS
+#' List columns available for analysis
 #'
 #' @param dataset Character vector of length 1. Identifies the dataset to be fetched. Use \code{list_apde_data} for available options
 #' @param analytic_only logical. Controls whether columns outside the analytic dataset should be returned.
-#'
-#'
 #' @return Data.frame with two columns. First column is the variable name, while the second identifies whether or not it is in the analytic ready dataset
 #' @export
 #'
@@ -31,11 +29,13 @@ list_apde_data <- function(){
 #' }
 list_dataset_columns <- function(dataset, analytic_only = F){
   dat = match.arg(dataset, list_apde_data())
-
+  
   warning('list_dataset_columns not currently available/implemented')
   return(data.frame(variable_name = '', analytic_ready = 'Sure. Why not?'))
-
+  
 }
+
+
 
 #' List of available metrics for calculation
 #' @export
@@ -47,6 +47,35 @@ survey_metrics = function(){
 record_metrics = function(){
   c('mean', 'median', 'sum', 'rate', 'se', 'lower', 'upper', 'rse', 'numerator', 'denominator', 'missing', 'missing.prop', 'total', 'distinct')
 }
+
+
+
+#' Improved rounding function
+#' @export
+#' @return numeric
+round2 = function(x, n = 0) {
+  posneg = sign(x)
+  z = abs(x)*10^n
+  z = z + 0.5
+  z = trunc(z)
+  z = z/10^n
+  z*posneg
+}
+
+
+#' Substring selection from the right to complement base R substr
+#' @export
+#' @return character vector
+#' 
+#' @examples 
+#' \dontrun{
+#' substrRight("Good morning!", 2, 8)
+#' }
+substrRight <- function(x, x.start, x.stop){
+  substr(x, nchar(x)-x.stop+1, nchar(x)-x.start+1)
+}
+
+
 
 
 #' Compare tabular results to a reference set of results in the same data
@@ -89,31 +118,6 @@ chi_compare <- function(orig,
 }
 
 
-#' Improved rounding function
-#' @export
-#' @return numeric
-round2 = function(x, n = 0) {
-  posneg = sign(x)
-  z = abs(x)*10^n
-  z = z + 0.5
-  z = trunc(z)
-  z = z/10^n
-  z*posneg
-}
-
-
-#' Substring selection from the right to complement base R substr
-#' @export
-#' @return character vector
-#' 
-#' @examples 
-#' \dontrun{
-#' substrRight("Good morning!", 2, 8)
-#' }
-substrRight <- function(x, x.start, x.stop){
-  substr(x, nchar(x)-x.stop+1, nchar(x)-x.start+1)
-}
-
 
 #' format list of years into a well formatted string
 #' @export
@@ -140,3 +144,5 @@ format.years <- function(temp){
   }
   return(new)
 }
+
+
