ce67938f72de6d1d24bb858131c15d6a4146b61a
Danny Colombara <48296707+dcolombara@users.noreply.github.com>
2021-07-01 13:58:35 -0700

    Added get_population
      - Added get_population()
      - Added vignette
      - Updated get_population tests
      - Updated documentation with devtools::document
      - Passed devtools::check

--- tests/testthat/test_get_population.R
+++ tests/testthat/test_get_population.R
@@ -7,9 +7,9 @@ test_that('get_population',{
 
   expect_equal(2153700, sum(get_population(years = 2017, genders = c("female", "male"))[]$pop) ) # 2017 KC (by summing both genders)
 
-  expect_equal(1077558, get_population(years = 2017, genders = "female")[]$pop ) # KC females 2017
+  expect_equal(1077304, get_population(years = 2017, genders = "female")[]$pop ) # KC females 2017
 
-  expect_equal(1076142, get_population(years = 2017, genders = "male")[]$pop ) # KC males 2017
+  expect_equal(1076396, get_population(years = 2017, genders = "male")[]$pop ) # KC males 2017
 
   expect_equal(2153700, sum(get_population(years = 2017, race_type = "race_eth", group_by = "race_eth")[]$pop) ) # 2017 KC (by summing all race_eth estimates)
 
