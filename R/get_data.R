7369e9adc930bc52a76a0330bdc174f95ea05274
Danny Colombara <48296707+dcolombara@users.noreply.github.com>
2021-07-06 09:56:51 -0700

    Address issue #112
      - still needs to be corrected for HYS data

--- R/get_data.R
+++ R/get_data.R
@@ -110,19 +110,30 @@ get_data_hys <- function(cols = NA, year = c(2016, 2018), weight_variable = 'kcf
 #'  get_data_birth(cols = NA, year = c(2015, 2016, 2017), kingco = F)
 #' }
 get_data_birth <- function(cols = NA, year = c(2017),  kingco = T){
-  # pull columns and years from sQL
-  ifelse(is.na(cols), cols <- "*", cols <- paste(cols, collapse=", "))
+  # get list of all colnames from SQL
+    con <- odbc::dbConnect(odbc::odbc(),
+                           Driver = "SQL Server",
+                           Server = "KCITSQLPRPDBM50",
+                           Database = "PH_APDEStore")
+    birth.names <- names(DBI::dbGetQuery(con, "SELECT top (0) * FROM [PH_APDEStore].[final].[bir_wa]"))
+
+  # pull columns and years from SQL
+    if(!(length(cols) == 1 & is.na(cols[1]))){
+      invalid.cols <- setdiff(cols, birth.names)
+      valid.cols <- intersect(birth.names, cols)
+    }
+
+    if(!(length(cols) == 1 & is.na(cols[1]))){
+      if(length(valid.cols) > 0){cols <- paste(valid.cols, collapse=", ")}
+      if(length(valid.cols) == 0){stop("Birth data cannot be extracted because no valid column names have been submitted. To get all columns, use the argument 'cols = NA'")}
+      if(length(invalid.cols) > 0){message(paste0("The following column names do not exist in the birth data and have not be extracted: ", paste0(invalid.cols, collapse = ", ")))}
+    }
+    if(length(cols) == 1 & is.na(cols[1])){cols <- "*"}
 
   query.string <- glue:: glue_sql ("SELECT ",  cols, " FROM [PH_APDEStore].[final].[bir_wa]
                                    WHERE chi_year IN (",  paste(year, collapse=", "), ")")
 
   if(kingco == T){query.string <- glue:: glue_sql (query.string, " AND chi_geo_kc = 1")}
-
-
-  con <- odbc::dbConnect(odbc::odbc(),
-                         Driver = "SQL Server",
-                         Server = "KCITSQLPRPDBM50",
-                         Database = "PH_APDEStore")
 
   dat <- data.table::setDT(DBI::dbGetQuery(con, query.string))
   odbc::dbDisconnect(con)
