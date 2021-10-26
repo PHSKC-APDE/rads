acb79c476da380f98aa6472b585313da38bd05b6
Daniel Casey <dcasey@kingcounty.gov>
2021-10-26 09:10:21 -0700

    wiki updater v2.0

--- wiki/update_wiki_files.R
+++ wiki/update_wiki_files.R
@@ -1,11 +1,44 @@
 library('knitr')
 
+#start in the rads directory
+start_dir = getwd()
 rmds = list.files('vignettes', '\\.Rmd$', full.names = T)
+rmds = file.path(start_dir, rmds)
 
+td = tempdir()
 
+wiki = file.path(td, 'rads.wiki')
+
+setwd(td)
+system('git clone https://github.com/PHSKC-APDE/rads.wiki.git')
+setwd(wiki)
+
+#Knit
 for(rrr in rmds){
   print(rrr)
+  out = paste0(tools::file_path_sans_ext(basename(rrr)), '.md')
+  knitr::knit(rrr, out)
+  file.copy(out, file.path(start_dir, 'wiki', basename(out)))
 
-  knitr::knit(rrr, file.path('wiki', paste0(tools::file_path_sans_ext(basename(rrr)), '.md')))
+  #clear out the headers
+  rl = readLines(out)
+
+  if(rl[1] == '---'){
+    nlines = which(rl == '---')
+    rl = rl[seq(max(nlines)+1, length(rl))]
+    writeLines(rl, con = out)
+  }
 
 }
+
+
+
+
+
+#push changes
+system('git add .')
+system(paste("git commit -m", shQuote('update wiki from automated process')))
+system('git push origin master')
+
+
+unlist(wiki)
