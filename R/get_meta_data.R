ba9fd083eb0291093ebf37a75375a8a70c229462
Buie <rbuie@ph.lcl>
2022-05-19 17:19:12 -0700

    add function "get_meta_data"

---
+++ R/get_meta_data.R
@@ -0,0 +1,25 @@
+#' Get meta data about variables available via get_data.
+#'
+#' @description Front end for querying meta-data tables. Currently only a few variables from "hys" are implemented.
+#'
+#' @param data_source Character vector. Identifies the dataset(s) to match variable names to. Default NA will report from all data-sets
+#' @param var_name Character vector. Identifies variable names to report the meta data of. Default NA will return information for all variables available
+#'
+#' @return data.table containing 2+n columns, naming the variable, the data source, and all available meta data
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#'  get_meta_data(dataset = "hys")
+#' }
+get_meta_data <- function(data_sources = NA, variable_names = NA){
+
+  DT <- data.table::as.data.table(rads:::metadata)
+  if(!is.na(data_sources)[1] & !is.na(variable_names)[1]) {
+    DT <- DT[data_source %in% data_sources & variable_name %in% variable_names]
+  } else if (!is.na(data_sources)) {
+
+  } else if (!is.na(variable_names)) {
+  }
+  return(DT)
+}
--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,4 +1,4 @@
 ## code to prepare `DATASET` dataset goes here
-metadata <- read.csv("./data-raw/metadata.csv")
+metadata <- data.table::fread("./data-raw/metadata.csv")
 
 usethis::use_data(metadata, internal = TRUE, overwrite = TRUE)
