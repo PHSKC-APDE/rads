85a8cfc22a069678ba96995ff75cf0d55feae2dd
Buie <rbuie@kingcounty.gov>
2025-04-18 17:28:28 -0700

    skeleton for modeler test

---
+++ tests/testthat/test_data_modeler.R
@@ -0,0 +1,10 @@
+testthat("data_modeler models currently known formats", {
+
+})
+testthat("data_modeler handles apostrophie and quote marks", {
+  # tet if at least abors and gives notice of why
+
+  # test if gracefully captures and models it
+
+
+})
