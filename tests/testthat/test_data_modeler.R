613360a73f7f5aeb42a83e31c0b892e942538fc0
Buie <rbuie@kingcounty.gov>
2026-01-08 13:57:17 -0800

    added baser dates to test

--- tests/testthat/test_data_modeler.R
+++ tests/testthat/test_data_modeler.R
@@ -1,6 +1,6 @@
 
 
-test_that("data_modeler creates correct types", {
+test_that("data_modeler creates matching types", {
   observations <- 1000
   DTTest <- data.table(
     id = 1:observations,
@@ -11,6 +11,7 @@ test_that("data_modeler creates correct types", {
     indicator1 = as.factor(sample(c("never","sometimes", "always", NA), observations, replace = T)),
     indicator2 = as.factor(sample(c(1,2,3,4, NA), observations, replace = T)),
     indicator3 = as.factor(sample(c("<20","21-40","41-60","61<"),  observations, replace = T)),
+    dates_baser = as.Date(sample(c("2025-01-01", "2025-06-01","2025-12-01"), observations, replace = T, prob = c(.33, .33, .33))),
     dates_posix14 = as.POSIXct(sample(c("2025-01-15 00:00:00 UTC","2025-02-15 00:00:00 UTC","2025-03-15 00:00:00 UTC","2025-04-15 00:00:00 UTC","2025-05-15 00:00:00 UTC","2025-06-15 00:00:00 UTC","2025-07-15 00:00:00 UTC","2025-08-15 00:00:00 UTC","2025-09-15 00:00:00 UTC","2025-10-15 00:00:00 UTC","2025-11-15 00:00:00 UTC","2025-12-15 00:00:00 UTC","2026-01-15 00:00:00 UTC","2026-02-15 00:00:00 UTC"), observations, replace = TRUE, prob = c(.05,.05,.05,.05,.05,.10,.10,.10,.10,.10,.10,.15,.05,.05)), tz = "UTC"))
 
   DTResult <- data_modeler(ph.data = DTTest, number_of_observations = 100, comments = T, return_code = F, print_code = T)
@@ -19,7 +20,7 @@ test_that("data_modeler creates correct types", {
   data_types_result <- sapply(DTResult, class)
 
   #all types match
-  testthat::expect_equal(all(unlist(data_types_test) == unlist(data_types_result)), TRUE)
+  expect_equal(all(unlist(data_types_test) == unlist(data_types_result)), TRUE)
 
 })
 
