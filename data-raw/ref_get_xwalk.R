aff6feca9fa37f1fcd7b2f569bb2254362fe06aa
Danny Colombara <dcolombara@kingcounty.gov>
2026-07-29 15:06:40 -0700

    remove get_xwalk and list_ref_xwalk
    - woefully out of date and time consuming to update regularly
    - Also only relevant to KC and trying to make rads generalized
    - all crosswalks available via rads.data anyway
    - cleaned out globals no longer needed
    - Passed all tests
      0 errors ✔ | 0 warnings ✔ | 0 notes ✔

--- data-raw/ref_get_xwalk.R
+++
@@ -1,9 +0,0 @@
-# Raw CSV (updated by hand when needed) lives in inst/extdata/ so it is
-# installed with the package. This script converts it to an *.rda file for
-# easy importation in R.
-
-ref_get_xwalk <- data.table::fread("inst/extdata/ref_get_xwalk.csv")
-
-save(list = "ref_get_xwalk", file = "data/ref_get_xwalk.rda" , compress = "bzip2", version = 3)
-
-usethis::use_data(ref_get_xwalk, overwrite = TRUE)
